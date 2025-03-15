6bbd03e5f4413828474dea59695086b17d700a7a
larshle <lars.leopold@hotmail.com>
2025-03-15 14:04:31 -0700

    file structure change

---
+++ build/gtest/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/googletest
+# Build directory: /home/cc/ee106b/sp25/class/ee106b-abk/Documents/206B-projects/project-3/build/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
